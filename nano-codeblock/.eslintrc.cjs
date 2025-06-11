b2143a037b3ce25ed6cd3a63fb559e4414d57611
kfir hashay <kfirhashay@outlook.com>
2025-06-11 15:07:18 +0300

    chore: add base configs

---
+++ nano-codeblock/.eslintrc.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+  root: true,
+  extends: ['eslint:recommended', 'plugin:import/recommended'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+};
